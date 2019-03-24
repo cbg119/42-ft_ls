dec6cd8ce2e59087078f26626e281c85f4b37fa8
Christian Bagdon <cbagdon119@gmail.com>
2019-03-24 10:53:49 -0700

    Made ft_lstpush actually work, lol

--- libft/includes/libft.h
+++ libft/includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: cbagdon <cbagdon@student.42.us.org>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/13 17:58:47 by cbagdon           #+#    #+#             */
-/*   Updated: 2019/03/22 02:18:14 by cbagdon          ###   ########.fr       */
+/*   Updated: 2019/03/24 10:52:33 by cbagdon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -139,7 +139,7 @@ void				ft_lstdelone(t_list **alst, void (*del)(void *, size_t));
 void				ft_lstdel(t_list **alst, void (*del)(void *, size_t));
 void				ft_lstadd(t_list **alst, t_list *new);
 void				ft_lstiter(t_list *lst, void (*f)(t_list *elem));
-void				ft_lstpush(t_list *head, t_list *new);
+void				ft_lstpush(t_list **head, t_list *new);
 t_list				*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
 
 /*
--- libft/lst/ft_lstpush.c
+++ libft/lst/ft_lstpush.c
@@ -6,20 +6,20 @@
 /*   By: cbagdon <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/13 16:52:45 by cbagdon           #+#    #+#             */
-/*   Updated: 2019/02/13 17:08:14 by cbagdon          ###   ########.fr       */
+/*   Updated: 2019/03/24 10:52:08 by cbagdon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/libft.h"
 
-void	ft_lstpush(t_list *head, t_list *new)
+void	ft_lstpush(t_list **head, t_list *new)
 {
-	if (head)
+	if (*head)
 	{
-		while (head->next)
-			head = head->next;
-		head->next = new;
+		while ((*head)->next)
+			*head = (*head)->next;
+		(*head)->next = new;
 	}
 	else
-		head = new;
+		*head = new;
 }
